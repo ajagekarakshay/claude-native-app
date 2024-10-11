246a4ee7cb573f0ef4ae657fc7f9a92dda8e1b29
Akshay <ajagekar.akshay@gmail.com>
2024-10-11 18:43:49 +0530

    prettier eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
+    "parserOptions": {
+      "ecmaVersion": "latest"
+    },
+    "env": {
+      "node": true
+    }
+  };
